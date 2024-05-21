4d86010897ce0b16f9bf0cb2d3fc7b3205a6ef6c
YashaHirekhan <yashahirekhan@gmail.com>
2024-05-21 13:11:18 +0530

    Updated the Page component

--- src/App.js
+++ src/App.js
@@ -7,12 +7,27 @@ export default function App() {
   const canvas = useRef(null)
   useEffect(function () {
     let c = canvas.current
+
+    const options = {
+      SIM_RESOLUTION: 12.8,
+      DENSITY_DISSIPATION: 1,
+      VELOCITY_DISSIPATION: 0.3,
+      PRESSURE: 0.8,
+      PRESSURE_ITERATIONS: 20,
+      CURL: 30,
+      SPLAT_RADIUS: 0.30,
+      SPLAT_FORCE: 6000,
+
+    };
+
     Fluid(c)
   }, [])
   return (
     <div className="App">
-      <canvas ref={canvas} style={{width: '100%', height: '100%'}}></canvas>
-      <div className='ontop'><Page/></div>
+      <canvas ref={canvas} style={{width: '100%', height: '350%'}}></canvas>
+      <div className='ontop'>
+        <Page/>
+      </div>
       
     </div>
   );
